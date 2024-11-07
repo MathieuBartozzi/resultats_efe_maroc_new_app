25a0b3f069fa22c5334cdcd024855130936e80d8
Mathieu Bartozzi <mathieu.bartozzi@mlfmonde.org>
2024-11-07 16:00:05 +0100

    orthographe mineure

--- ACCUEIL.py
+++ ACCUEIL.py
@@ -11,12 +11,11 @@ st.title("Analyse des Résultats des Épreuves - EFE Maroc")
 
 # Introduction
 st.markdown("""
-Bienvenue sur l'application d'analyse des résultats des épreuves du Baccalauréat et du Diplôme National du Brevet (DNB) pour les établissements EFE Maroc.
-Cette interface vous permet de naviguer et d'explorer les données de performance des élèves par épreuve, par spécialité, et par établissement pour les années 2023 et 2024.
+Cette application permet d'analyser les résultats des épreuves du Baccalauréat et du Diplôme National du Brevet (DNB) pour les établissements EFE Maroc.
+Cette interface vous permet de naviguer et d'explorer les données de performance des établissement par épreuve pour les années 2023 et 2024.
 
-Chaque section présente des graphiques interactifs et des classements qui permettent de visualiser les moyennes des différents établissements, ainsi que les variations d'une année à l'autre. Vous pouvez également mettre en surbrillance un établissement de votre choix pour comparer ses résultats aux autres établissements dans des épreuves spécifiques. En plus, une analyse des corrélations entre les épreuves est disponible, vous offrant un aperçu des tendances et des performances comparatives des élèves.
+Chaque section présente des graphiques interactifs et des classements qui permettent de visualiser les moyennes des différents établissements, ainsi que les variations d'une année à l'autre. Un filtrage par établissement de votre choix pour comparer ses résultats aux autres établissements dans des épreuves spécifiques.
 
-Explorez les différents onglets et visualisations pour découvrir en détail les résultats académiques de chaque épreuve, et n'hésitez pas à utiliser les options de filtrage pour personnaliser votre analyse.
 """)
 
 st.write('**Bonne navigation !**')
