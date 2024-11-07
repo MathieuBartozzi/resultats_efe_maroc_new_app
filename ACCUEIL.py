7976e9d7f18349096be43130abff1f6ddfcbe774
Mathieu Bartozzi <mathieu.bartozzi@mlfmonde.org>
2024-11-07 15:57:22 +0100

    orthographe mineure

--- ACCUEIL.py
+++ ACCUEIL.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(
     page_title='Résultats EFE Maroc')
 
-st.sidebar.success("Selectionner une page au-dessus.")
+st.sidebar.success("Selectionner une page.")
 
 # Titre de l'application
 st.title("Analyse des Résultats des Épreuves - EFE Maroc")
